151bbd87dd9875f41228df2fb023ced145d7271b
Madina <hamitmadina2005@gmail.com>
2025-06-04 16:31:20 +0500

    LAB2

---
+++ lab2py/Lists.py
@@ -0,0 +1,8 @@
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
